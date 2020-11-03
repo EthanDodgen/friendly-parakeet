aa1aab7f83477f72c1e35cbd465de8dc0c5383fe
fival <thefivalco@gmail.com>
2020-11-02 21:07:08 -0600

    randm_start

--- friendly-parakeet/Develop/script.js
+++ friendly-parakeet/Develop/script.js
@@ -1,3 +1,5 @@
+//final steps. Randomly generate answer variables, make generate button begin function, have answer array or password appear in application
+
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var numeric = ["0","1","2","3","4","5","6","7","8","9"]
@@ -14,7 +16,7 @@ function criteria_prompt() {
 
   var characters = window.prompt("Would you like to include lowercase, uppercase, or both characters.")
     characters = characters.toLocaleLowerCase()
-    if ((characters === "lowercase") || (characters === "LOWERCASE") || (characters === "uppercase") || (characters === "UPPERCASE") || (characters === "both") || (characters === "BOTH")) {
+    if ((characters === "lowercase") || (characters === "uppercase") || (characters === "both")) {
       window.alert("Are you sure you want " + characters + "?")
       console.log(characters)
     }else {
@@ -22,7 +24,14 @@ function criteria_prompt() {
       console.log("no match found")
     }
     if (characters === "lowercase")
-      answer.push(lowercase[0])
+    //create random case
+      answer.push(lowercase = Math.floor(Math.random() * 20) + 1)
+      console.log(answer)
+    if (characters === "uppercase")
+      answer.push(uppercase[0])
+      console.log(answer)
+    if (characters === "both")
+      answer.push(lowercase[0], uppercase[0])
       console.log(answer)
   }
 criteria_prompt();
@@ -38,11 +47,14 @@ function numeric_prompt() {
     }else {
       numeric_prompt()
     }
+    if (number === "yes")
+    //create random numeric
     answer.push(numeric[0])
+    console.log(answer)
 }
 numeric_prompt();
 
-function special_characters() {
+function special_characters() { 
 
   var special = window.prompt("Would you like to use special characters?") 
 
@@ -52,11 +64,12 @@ function special_characters() {
   }else {
     special_characters()
   }
+  if (special === "yes")
+  //create random symbol
   answer.push(symbol[0])
+  console.log(answer)
 }
 special_characters();
-
-
 
 
 // Get references to the #generate element
