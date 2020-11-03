014590a895ee9d22759e170ffc138118bb5f4f96
Fival <thefivalco@gmail.com>
2020-11-03 13:36:43 -0600

    changes

--- Develop/script.js
+++ Develop/script.js
@@ -7,91 +7,93 @@ var symbol = ["#","$","%","&","+","-",":","?"]
 
 var answer = []
 
-//obtain password criteria
-
-var length = window.prompt("Between 8 - 128, how long would you like your password?")
-console.log(length)
-
-function criteria_prompt() {
-
-  var characters = window.prompt("Would you like to include lowercase, uppercase, or both characters.")
-    characters = characters.toLocaleLowerCase()
-
-    if ((characters === "lowercase") || (characters === "uppercase") || (characters === "both")) {
-      window.alert("Are you sure you want " + characters + "?")
-      console.log(characters)
-    }else {
-      criteria_prompt()
-    }
-    if (characters === "lowercase") {
-      var random_index = Math.floor(Math.random() * 25)
-        answer.push(lowercase[random_index])
-    }
-    if (characters === "uppercase") {
-      var random_index = Math.floor(Math.random() * 25)
-      answer.push(uppercase[random_index])
-    }
-    if (characters === "both") {
-      var random_index = Math.floor(Math.random() * 25)
-      answer.push(lowercase[random_index], uppercase[random_index])
-    }
-      console.log(answer)
-  }
-criteria_prompt();
 
 
-function numeric_prompt() {
+//var password = generatePassword();
 
-  var number = window.prompt("Would you like numeric characters?")
-    number = number.toLocaleLowerCase()
+//var passwordText = document.querySelector("#password");
 
-    if ((number === "yes") || (number === "no")){
-      window.alert("Are you sure you meant " + number + "?")
-      console.log(number)
-    }else {
-      numeric_prompt()
-    }
-    if (number === "yes") {
-      var random_number = Math.floor(Math.random() * 10)
-      answer.push(numeric[random_number])
-    }
-    console.log(answer)
-}
-numeric_prompt();
-
-function special_characters() { 
-
-  var special = window.prompt("Would you like to use special characters?")
-    special = special.toLocaleLowerCase() 
-
-  if ((special === "yes") || (special === "no")){
-    window.alert("Are you sure you meant " + special + "?")
-    console.log(special)
-    }else {
-    special_characters()
-  }
-  if (special === "yes") {
-    var random_special = Math.floor(Math.random() * 8)
-    answer.push(symbol[random_special])
-  }
-  console.log(answer)
-}
-special_characters();
+//passwordText.value = password;
 
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
 
 
-// Write password to the #password input
 function writePassword() {
 
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var length = window.prompt("Between 8 - 128, how long would you like your password?")
+  console.log(length)
 
-  passwordText.value = password;
+  function criteria_prompt() {
 
+    var characters = window.prompt("Would you like to include lowercase, uppercase, or both characters.")
+      characters = characters.toLocaleLowerCase()
+
+      if ((characters === "lowercase") || (characters === "uppercase") || (characters === "both")) {
+        window.alert("Are you sure you want " + characters + "?")
+        console.log(characters)
+      }else {
+        criteria_prompt()
+      }
+      if (characters === "lowercase") {
+        var random_index = Math.floor(Math.random() * 25)
+          answer.push(lowercase[random_index])
+      }
+      if (characters === "uppercase") {
+        var random_index = Math.floor(Math.random() * 25)
+        answer.push(uppercase[random_index])
+      }
+      if (characters === "both") {
+        var random_index = Math.floor(Math.random() * 25)
+        answer.push(lowercase[random_index], uppercase[random_index])
+      }
+        console.log(answer)
+    }
+  criteria_prompt();
+
+
+  function numeric_prompt() {
+
+    var number = window.prompt("Would you like numeric characters?")
+      number = number.toLocaleLowerCase()
+
+      if ((number === "yes") || (number === "no")){
+        window.alert("Are you sure you meant " + number + "?")
+        console.log(number)
+      }else {
+        numeric_prompt()
+      }
+      if (number === "yes") {
+        var random_number = Math.floor(Math.random() * 10)
+        answer.push(numeric[random_number])
+      }
+      console.log(answer)
+  }
+  numeric_prompt();
+
+  function special_characters() { 
+
+    var special = window.prompt("Would you like to use special characters?")
+      special = special.toLocaleLowerCase() 
+
+    if ((special === "yes") || (special === "no")){
+      window.alert("Are you sure you meant " + special + "?")
+      console.log(special)
+      }else {
+      special_characters()
+    }
+    if (special === "yes") {
+      var random_special = Math.floor(Math.random() * 8)
+      answer.push(symbol[random_special])
+    }
+    console.log(answer)
+  }
+  special_characters();
 }
+writePassword();
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePasswordanswer);
+// Get references to the #generate element
+
+var generateBtn = document.querySelector("#generate");
+console.log(generateBtn)
+
+generateBtn.addEventListener("click", writePassword); 
