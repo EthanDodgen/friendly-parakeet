9334d8b738bea16afb9862f0133aedb46133af46
Fival <thefivalco@gmail.com>
2020-11-03 10:12:37 -0600

    changes

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,5 @@
+//final steps. Randomly generate answer variables, make generate button begin function, have answer array or password appear in application
+
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var numeric = ["0","1","2","3","4","5","6","7","8","9"]
@@ -14,15 +16,18 @@ function criteria_prompt() {
 
   var characters = window.prompt("Would you like to include lowercase, uppercase, or both characters.")
     characters = characters.toLocaleLowerCase()
-    if ((characters === "lowercase") || (characters === "LOWERCASE") || (characters === "uppercase") || (characters === "UPPERCASE") || (characters === "both") || (characters === "BOTH")) {
+    if ((characters === "lowercase") || (characters === "uppercase") || (characters === "both")) {
       window.alert("Are you sure you want " + characters + "?")
       console.log(characters)
     }else {
       criteria_prompt()
-      console.log("no match found")
     }
     if (characters === "lowercase")
-      answer.push(lowercase[0])
+        answer.push(lowercase = Math.floor(Math.random() * 20) + 1)
+    if (characters === "uppercase")
+      answer.push(uppercase[0])
+    if (characters === "both")
+      answer.push(lowercase[0], uppercase[0])
       console.log(answer)
   }
 criteria_prompt();
@@ -38,25 +43,28 @@ function numeric_prompt() {
     }else {
       numeric_prompt()
     }
-    answer.push(numeric[0])
+    if (number === "yes")
+    answer.push(numeric = Math.floor(Math.random() * 20) + 1)
+    console.log(answer)
 }
 numeric_prompt();
 
-function special_characters() {
+function special_characters() { 
 
   var special = window.prompt("Would you like to use special characters?") 
 
   if ((special === "yes") || (special === "no")){
     window.alert("Are you sure you meant " + special + "?")
     console.log(special)
-  }else {
+    }else {
     special_characters()
   }
-  answer.push(symbol[0])
+  if (special === "yes")
+  answer.push(special = Math.floor(Math.random() * 20) + 1)
+  console.log(answer)
+
 }
 special_characters();
-
-
 
 
 // Get references to the #generate element
